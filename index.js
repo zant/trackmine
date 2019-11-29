46dd66c4e029b202bf66874966df8e98fdf023f2
gonza <gonzalo.christobal@gmail.com>
2019-11-29 15:43:34 -0300

    1st commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const trackme = require('commander');
+const prompt = require('prompt');
+const URL = 'redmine.codium.com.py';
+const http = require('http');
+const FormData = require('form-data');
+require('dotenv').config();
+
+trackme
+  .version('0.0.1')
+  .name('track')
+  .requiredOption('-i, --issue <issue>', 'issue')
+  .requiredOption(
+    '-w, --when <time>',
+    "if no option provided it will use today's date",
+    new Date().toISOString().split('T')[0]
+  )
+  .requiredOption('-h, --hours <hours>', 'amount of hours')
+  .option('-c, --comments <comments>', 'commentario')
+  .option('-a, --activity <activity_id>', 'activity id')
+  .requiredOption(
+    '-f --fabrica',
+    'if it was at fabrica, this is the default behaviour',
+    true
+  )
+  .parse(process.argv);
+
+//create form data
+const data = createFormData(trackme);
+
+//post
+postData(URL, data);
+
+function createFormData(trackme) {
+  return (({ project, issue, when, hours, comments, activity, fabrica }) => {
+    return new FormData({
+      time_entry: {
+        issue_id: issue,
+        spent_on: when,
+        hours,
+        comments,
+        activity_id: activity,
+        custom_field_values: {
+          '15': (fabrica => (fabrica ? 'Fábrica' : 'Cliente'))(fabrica)
+        }
+      }
+    });
+  })(trackme);
+}
+
+function postData(url, data) {
+  const request = http.request({
+    hostname: url,
+    method: 'POST',
+    path: '/time_entries.xml',
+    headers: {
+      'X-Redmine-API-Key': process.env.API_KEY
+    }
+  });
+
+  data.pipe(request);
+
+  request.on('response', res => {
+    console.log(res.statusCode);
+    console.log(res.statusMessage);
+  });
+
+  request.on('error', e => {
+    console.log(e);
+    process.exit(1);
+  });
+
+  request.end();
+}
